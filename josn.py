d055fd75a0f0707580a1efee4a4a6e1e76295853
vaishali tiwari <136571044+maahi104@users.noreply.github.com>
2023-07-21 23:53:22 +0530

    Update josn.py

--- josn.py
+++ josn.py
@@ -1,17 +1,1 @@
-import json
-
-# Create a dictionary of Indian states and their capitals
-states = {
-    'Andhra Pradesh': 'Amaravati',
-    'Gujarat': 'Gandhinagar',
-    'Karnataka': 'Bengaluru',
-    'Maharashtra': 'Mumbai',
-    'Odisha': 'Bhubaneswar',
-    'Rajasthan': 'Jaipur',
-    'Tamil Nadu': 'Chennai'
-}
-
-# Write the dictionary to a JSON file
-with open('indian_states.json', 'w') as f:
-    json.dump(states, f)
 
